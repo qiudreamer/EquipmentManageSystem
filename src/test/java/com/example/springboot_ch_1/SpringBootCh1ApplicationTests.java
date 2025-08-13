ff473e2216094506264dc9690c6eee78c3f5ba31
qiudreamer <1243139737@qq.com>
2025-08-13 15:18:27 +0800

    初始版本

---
+++ src/test/java/com/example/springboot_ch_1/SpringBootCh1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_ch_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCh1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
